daa57e955b071d48173917ac557ef417be9802ad
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-05-28 09:51:07 -0300

    Create a begenning project

---
+++ src/main/java/com/devjoliveira/jocommerce/JocommerceApplication.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.jocommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JocommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JocommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjoliveira/jocommerce/JocommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.jocommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JocommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
